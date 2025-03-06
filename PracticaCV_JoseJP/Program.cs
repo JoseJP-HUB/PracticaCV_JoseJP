e87de1c928e82c7e86bd986a80209da71e83ee99
Jose Jimenez <barplazamariadelcarmen@gmail.com>
2025-03-06 13:44:53 +0100

    Establecimiento de la estructura

--- PracticaCV_JoseJP/Program.cs
+++ PracticaCV_JoseJP/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Variables
+
+            //Incicializamos la clase
+
+            //Entradas
+
+            //Procesos
+
+            //Salidas
         }
     }
 }
